fa45bd683e1baa8de1351ff9b2be471879f7466a
Antoine <antoine.cellier@zenika.com>
2022-02-14 14:43:57 +0100

    :config: add linter for typescript
    Conflicts:
    	components/MessageInput/index.tsx

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
       files: ['*.ts', '*.tsx'],
       rules: {
         '@typescript-eslint/no-shadow': ['error'],
+        '@typescript-eslint/no-explicit-any': ['warn'],
         'no-shadow': 'off',
         'no-undef': 'off',
       },
